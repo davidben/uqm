713174a65b8daa761ef1b2e307f31d6a116f981f
meep-eep <meep-eep@8092fc87-c524-0410-9efc-e669fe64eaf9>
2002-09-22 18:17:47 +0000

    Some cleanup.
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@65 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/sc2code/dummy.c
+++ src/sc2code/dummy.c
@@ -98,52 +98,35 @@ enum
 			}
 			case BLACKURQ_CODE_RES:
 			{
-				extern RACE_DESCPTR
-						init_black_urquan
-						(void);
+				extern RACE_DESCPTR init_black_urquan (void);
 
 				RDPtr = init_black_urquan ();
 				break;
 			}
 			case CHENJESU_CODE_RES:
 			{
-				extern
-						RACE_DESCPTR
-						init_chenjesu
-						(void);
+				extern RACE_DESCPTR init_chenjesu (void);
 
 				RDPtr = init_chenjesu ();
 				break;
 			}
 			case CHMMR_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_chmmr
-						(void);
+				extern RACE_DESCPTR init_chmmr (void);
 
 				RDPtr = init_chmmr ();
 				break;
 			}
 			case DRUUGE_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_druuge
-						(void);
+				extern RACE_DESCPTR init_druuge (void);
 
 				RDPtr = init_druuge ();
 				break;
 			}
 			case HUMAN_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_human
-						(void);
+				extern RACE_DESCPTR init_human (void);
 
 				RDPtr = init_human ();
 				break;
@@ -151,76 +134,49 @@ enum
 			case ILWRATH_CODE_RES:
 			{
 				
-						extern
-						RACE_DESCPTR
-						init_ilwrath
-						(void);
+				extern RACE_DESCPTR init_ilwrath (void);
 
 				RDPtr = init_ilwrath ();
 				break;
 			}
 			case MELNORME_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_melnorme
-						(void);
+				extern RACE_DESCPTR init_melnorme (void);
 
 				RDPtr = init_melnorme ();
 				break;
 			}
 			case MMRNMHRM_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_mmrnmhrm
-						(void);
+				extern RACE_DESCPTR init_mmrnmhrm (void);
 
 				RDPtr = init_mmrnmhrm ();
 				break;
 			}
 			case MYCON_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_mycon
-						(void);
+				extern RACE_DESCPTR init_mycon (void);
 
 				RDPtr = init_mycon ();
 				break;
 			}
 			case ORZ_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_orz
-						(void);
+				extern RACE_DESCPTR init_orz (void);
 
 				RDPtr = init_orz ();
 				break;
 			}
 			case PKUNK_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_pkunk
-						(void);
+				extern RACE_DESCPTR init_pkunk (void);
 
 				RDPtr = init_pkunk ();
 				break;
 			}
 			case SHOFIXTI_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_shofixti
-						(void);
+				extern RACE_DESCPTR init_shofixti (void);
 
 				RDPtr = init_shofixti ();
 				break;
@@ -228,87 +184,56 @@ enum
 			case SLYLANDR_CODE_RES:
 			{
 				
-						extern
-						RACE_DESCPTR
-						init_slylandro
-						(void);
+				extern RACE_DESCPTR init_slylandro (void);
 
 				RDPtr = init_slylandro ();
 				break;
 			}
 			case SPATHI_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_spathi
-						(void);
+				extern RACE_DESCPTR init_spathi (void);
 
 				RDPtr = init_spathi ();
 				break;
 			}
 			case SUPOX_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_supox
-						(void);
+				extern RACE_DESCPTR init_supox (void);
 
 				RDPtr = init_supox ();
 				break;
 			}
 			case SYREEN_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_syreen
-						(void);
+				extern RACE_DESCPTR init_syreen (void);
 
 				RDPtr = init_syreen ();
 				break;
 			}
 			case THRADD_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_thraddash
-						(void);
+				extern RACE_DESCPTR init_thraddash (void);
 
 				RDPtr = init_thraddash ();
 				break;
 			}
 			case UMGAH_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_umgah
-						(void);
+				extern RACE_DESCPTR init_umgah (void);
 
 				RDPtr = init_umgah ();
 				break;
 			}
 			case URQUAN_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_urquan
-						(void);
+				extern RACE_DESCPTR init_urquan (void);
 
 				RDPtr = init_urquan ();
 				break;
 			}
 			case UTWIG_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_utwig
-						(void);
+				extern RACE_DESCPTR init_utwig (void);
 
 				RDPtr = init_utwig ();
 				break;
@@ -316,65 +241,42 @@ enum
 			case VUX_CODE_RES:
 			{
 				
-						extern
-						RACE_DESCPTR
-						init_vux
-						(void);
+				extern RACE_DESCPTR init_vux (void);
 
 				RDPtr = init_vux ();
 				break;
 			}
 			case YEHAT_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_yehat
-						(void);
+				extern RACE_DESCPTR init_yehat (void);
 
 				RDPtr = init_yehat ();
 				break;
 			}
 			case ZOQFOT_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_zoqfotpik
-						(void);
+				extern RACE_DESCPTR init_zoqfotpik (void);
 
 				RDPtr = init_zoqfotpik ();
 				break;
 			}
 			case SAMATRA_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_samatra
-						(void);
+				extern RACE_DESCPTR init_samatra (void);
 
 				RDPtr = init_samatra ();
 				break;
 			}
 			case SIS_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_sis
-						(void);
+				extern RACE_DESCPTR init_sis (void);
 
 				RDPtr = init_sis ();
 				break;
 			}
 			case PROBE_CODE_RES:
 			{
-				
-						extern
-						RACE_DESCPTR
-						init_probe
-						(void);
+				extern RACE_DESCPTR init_probe (void);
 
 				RDPtr = init_probe ();
 				break;
@@ -435,7 +337,7 @@ CaptureCodeRes (MEM_HANDLE hCode, PVOID pData, PVOID *ppLocData)
 
 	if(hCode==0)
 	{
-		printf("Ack! dummy.c::CaptureCodeRes() hCode==0! FATAL!\n");
+		fprintf(stderr, "Ack! dummy.c::CaptureCodeRes() hCode==0! FATAL!\n");
 		return(0);
 	}
 
@@ -483,12 +385,5 @@ COUNT
 GetPrimNextLink (PPRIMITIVE pPrim)
 {
 	return (GetSuccLink (GetPrimLinks (pPrim)));
-}
-
-int
-abs (int n)
-{
-	if (n < 0) n = -n;
-	return (n);
 }
 
