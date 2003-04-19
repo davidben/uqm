f4e3eafff303a3c3583ca2a78506e4f1d73c1dab
gewlitys <gewlitys@8092fc87-c524-0410-9efc-e669fe64eaf9>
2003-04-19 12:45:33 +0000

    bumped version to 0.22
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@913 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/uqmversion.h
+++ src/uqmversion.h
@@ -18,7 +18,7 @@
 #define UQMVERSION_H 
 
 #define UQM_MAJOR_VERSION 0
-#define UQM_MINOR_VERSION 21
+#define UQM_MINOR_VERSION 22
 #define UQM_EXTRA_VERSION ""
 /* The final version is interpreted as:
    printf ("%d.%d%s", UQM_MAJOR_VERSION, UQM_MINOR_VERSION, UQM_EXTRA_VERSION);
