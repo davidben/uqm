d9d03a61cb5d2d3291e0f51d64fdc81bd27ebd5a
mcmartin <mcmartin@8092fc87-c524-0410-9efc-e669fe64eaf9>
2003-07-26 00:08:53 +0000

    Upgraded sound buffers to nearest power of two Linux's sound system demands this, it seems
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@1086 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/sc2code/libs/sound/mixsdl/sound_mixsdl.c
+++ src/sc2code/libs/sound/mixsdl/sound_mixsdl.c
@@ -85,19 +85,19 @@ TFB_mixSDL_InitSound (int driver, int flags)
 	{
 		audio_quality = MIX_QUALITY_HIGH;
 		audio_rate = 44100;
-		audio_bufsize = 3072;
+		audio_bufsize = 4096;
 	}
 	else if (flags & TFB_SOUNDFLAGS_LQAUDIO)
 	{
 		audio_quality = MIX_QUALITY_LOW;
 		audio_rate = 22050;
-		audio_bufsize = 1536;
+		audio_bufsize = 2048;
 	}
 	else
 	{
 		audio_quality = MIX_QUALITY_DEFAULT;
 		audio_rate = 44100;
-		audio_bufsize = 3072;
+		audio_bufsize = 4096;
 	}
 
 	fprintf (stderr, "Initializing MixSDL mixer.\n");
