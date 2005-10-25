405ae38fddfaa9fee4fcb7fda15ba44d2fda1f83
meep-eep <meep-eep@8092fc87-c524-0410-9efc-e669fe64eaf9>
2005-10-25 10:52:10 +0000

    Separate config files for MinGW/Cygwin.
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@1921 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/config.h
+++ src/config.h
@@ -7,6 +7,10 @@
 	 * If you want anything else than the defaults, you'll have to edit
 	 * that file manually. */
 #	include "msvc++/config.h"
+#elif defined (__MINGW32__) || defined (__CYGWIN__)
+	/* If we're compiling on MS Windows using build.sh, use
+	 * config_win.h, generated from * src/config_win.h.in. */
+#	include "config_win.h"
 #else
 	/* If we're compiling in unix, use config_unix.h, generated from
 	 * src/config_unix.h.in by build.sh. */
