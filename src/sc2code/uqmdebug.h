c4ecd6dd44f1e9a2595ac5702e8287526c01343b
avolkov <avolkov@8092fc87-c524-0410-9efc-e669fe64eaf9>
2005-11-07 18:08:23 +0000

    need USE_DEBUG_KEY here too
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@1961 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/sc2code/uqmdebug.h
+++ src/sc2code/uqmdebug.h
@@ -14,7 +14,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#if !defined(_DEBUG_H) && defined(DEBUG)
+#if !defined(_DEBUG_H) && (defined(DEBUG) || defined(USE_DEBUG_KEY))
 #define _DEBUG_H
 
 #include "clock.h"
