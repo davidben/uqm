0613e1824cbcdfc0bfd7ea8e8211190f0494e1df
avolkov <avolkov@8092fc87-c524-0410-9efc-e669fe64eaf9>
2005-12-07 16:51:23 +0000

    Fixes Pause dialog undrawing incorrectly during the ending sequences
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@2038 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/sc2code/util.c
+++ src/sc2code/util.c
@@ -149,6 +149,7 @@ PauseGame (void)
 	CONTEXT OldContext;
 	FRAME F;
 	HOT_SPOT OldHot;
+	RECT OldRect;
 
 	if (ActivityFrame == 0
 			|| (GLOBAL (CurrentActivity) & (CHECK_ABORT | CHECK_PAUSE))
@@ -166,6 +167,8 @@ PauseGame (void)
 	LockMutex (GraphicsLock);
 	OldContext = SetContext (ScreenContext);
 	OldHot = SetFrameHot (Screen, MAKE_HOT_SPOT (0, 0));
+	GetContextClipRect (&OldRect);
+	SetContextClipRect (NULL_PTR);
 
 	GetFrameRect (ActivityFrame, &r);
 	r.corner.x = (SCREEN_WIDTH - r.extent.width) >> 1;
@@ -197,6 +200,7 @@ PauseGame (void)
 	DrawStamp (&s);
 	DestroyDrawable (ReleaseDrawable (s.frame));
 
+	SetContextClipRect (&OldRect);
 	SetFrameHot (Screen, OldHot);
 	SetContext (OldContext);
 
