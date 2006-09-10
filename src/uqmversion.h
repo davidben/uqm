e5412271b4c5df6bd1f2c99092882e9e4ffd10e3
mcmartin <mcmartin@8092fc87-c524-0410-9efc-e669fe64eaf9>
2006-09-10 18:42:25 +0000

    Micro version bump to reflect Keyconfig and new VControl template version
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@2436 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/uqmversion.h
+++ src/uqmversion.h
@@ -21,8 +21,8 @@
 #define UQM_MAJOR_VERSION_S  "0"
 #define UQM_MINOR_VERSION     5
 #define UQM_MINOR_VERSION_S  "5"
-#define UQM_PATCH_VERSION     2
-#define UQM_PATCH_VERSION_S  "2"
+#define UQM_PATCH_VERSION     3
+#define UQM_PATCH_VERSION_S  "3"
 #define UQM_EXTRA_VERSION    ""
 /* The final version is interpreted as:
  * printf ("%d.%d.%d%s", UQM_MAJOR_VERSION, UQM_MINOR_VERSION,
