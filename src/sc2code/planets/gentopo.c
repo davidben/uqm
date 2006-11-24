705dcc99efc052df711dffd8339e6d28af70b64d
meep-eep <meep-eep@8092fc87-c524-0410-9efc-e669fe64eaf9>
2006-11-24 23:50:18 +0000

    Refer to doc/devel/planettopo
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@2534 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/sc2code/planets/gentopo.c
+++ src/sc2code/planets/gentopo.c
@@ -16,6 +16,8 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+// See doc/devel/planettopo for details.
+
 #include "gfxlib.h"
 #include "mathlib.h"
 #include "planets.h"
