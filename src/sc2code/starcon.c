a39f6f2bf65be1671dcab46ea732ba7418638608
mcmartin <mcmartin@8092fc87-c524-0410-9efc-e669fe64eaf9>
2007-01-14 03:38:27 +0000

    Froperly exit with failure if the content directory is found but the kernel data is not.
    This is necessary for the error messages to be visible in Windows.
    
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@2663 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/sc2code/starcon.c
+++ src/sc2code/starcon.c
@@ -281,6 +281,7 @@ while (--ac > 0)
 		log_add (log_Fatal, "This file is typically called uqm-%d.%d.0.uqm.  UQM was expecting it", UQM_MAJOR_VERSION, UQM_MINOR_VERSION);
 		log_add (log_Fatal, "in the %s/packages directory.", baseContentPath);
 		log_add (log_Fatal, "Either your installation did not install the content pack at all, or it\ninstalled it in a different directory.\n\nFix your installation and rerun UQM.\n\n  *******************\n");
+		exit (EXIT_FAILURE);
 	}
 	FreeKernel ();
 
