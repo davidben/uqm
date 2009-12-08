davidben/uqm

commit 59faa717a391ede0819814f78aebe6cdb464be19

Make context cliprect + foreground frame hotspot semantics actually sane; do not hack _CurFramePtr->HotSpot
git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@3401 8092fc87-c524-0410-9efc-e669fe64eaf9

src/libs/gfxlib.h
@@ -247,8 +247,11 @@ extern Color GetContextBackGroundColor (void);
 extern FRAME SetContextFGFrame (FRAME Frame);
 extern FRAME GetContextFGFrame (void);
 extern BOOLEAN SetContextClipping (BOOLEAN ClipStatus);
+// Context cliprect defines the drawing bounds. Additionally, all
+// drawing positions (x,y) are relative to the cliprect corner.
 extern BOOLEAN SetContextClipRect (RECT *pRect);
 extern BOOLEAN GetContextClipRect (RECT *pRect);
+
 extern TIME_VALUE DrawablesIntersect (INTERSECT_CONTROL *pControl0,
 		INTERSECT_CONTROL *pControl1, TIME_VALUE max_time_val);
 extern void DrawStamp (STAMP *pStamp);
@@ -264,7 +267,6 @@ extern void DrawBatch (PRIMITIVE *pBasePrim, PRIM_LINKS PrimLinks,
 extern void BatchGraphics (void);
 extern void UnbatchGraphics (void);
 extern void FlushGraphics (void);
-extern void ClearBackGround (RECT *pClipRect);
 extern void ClearDrawable (void);
 #ifdef DEBUG
 extern CONTEXT CreateContextAux (const char *name);
@@ -297,6 +299,7 @@ extern DRAWABLE LoadDisplayPixmap (RECT *area, FRAME frame);
 extern FRAME SetContextFontEffect (FRAME EffectFrame);
 extern FONT SetContextFont (FONT Font);
 extern BOOLEAN DestroyFont (FONT FontRef);
+// The returned pRect is relative to the context drawing origin
 extern BOOLEAN TextRect (TEXT *pText, RECT *pRect, BYTE *pdelta);
 extern BOOLEAN GetContextFontLeading (SIZE *pheight);
 extern BOOLEAN GetContextFontLeadingWidth (SIZE *pwidth);

src/libs/graphics/context.h
@@ -152,5 +152,8 @@ extern GRAPHICS_STATUS _GraphicsStatusFlags;
 		(_GraphicsStatusFlags & (GRAPHICS_STATUS)(GRAPHICS_ACTIVE \
 							| GRAPHICS_VISIBLE))
 
+// pValidRect or origin may be NULL
+BOOLEAN GetContextValidRect (RECT *pValidRect, POINT *origin);
+
 #endif /* _CONTEXT_H */
 

src/libs/graphics/drawable.h
@@ -57,6 +57,7 @@ struct drawable_desc
 
 #define GetFrameWidth(f) ((f)->Bounds.width)
 #define GetFrameHeight(f) ((f)->Bounds.height)
+#define GetFrameBounds(f) ((f)->Bounds)
 #define SetFrameBounds(f,w,h) \
 		((f)->Bounds.width=(w), \
 		((f))->Bounds.height=(h))
@@ -84,11 +85,10 @@ extern INTERSECT_CODE _clip_line (RECT *pClipRect, BRESENHAM_LINE *pLine);
 extern void *_GetCelData (uio_Stream *fp, DWORD length);
 extern BOOLEAN _ReleaseCelData (void *handle);
 
-extern STAMP _save_stamp;
 extern FRAME _CurFramePtr;
 
-extern void _rect_blt (RECT *pClipRect, PRIMITIVE *PrimPtr);
-extern void _text_blt (RECT *pClipRect, PRIMITIVE *PrimPtr);
+// ClipRect is relative to ctxOrigin
+extern void _text_blt (RECT *pClipRect, PRIMITIVE *PrimPtr, POINT ctxOrigin);
 
 #endif /* _DRAWABLE_H */
 

src/libs/graphics/font.c
@@ -58,7 +58,6 @@ font_DrawText (TEXT *lpText)
 	DrawBatch (&_locPrim, 0, BATCH_SINGLE);
 }
 
- 
 /* Draw the stroke by drawing the same text in the
  * background color one pixel shifted to all 4 directions.
  */
@@ -225,7 +224,7 @@ TextRect (TEXT *lpText, RECT *pRect, BYTE *pdelta)
 }
 
 void
-_text_blt (RECT *pClipRect, PRIMITIVE *PrimPtr)
+_text_blt (RECT *pClipRect, PRIMITIVE *PrimPtr, POINT ctxOrigin)
 {
 	FONT FontPtr;
 
@@ -244,7 +243,7 @@ _text_blt (RECT *pClipRect, PRIMITIVE *PrimPtr)
 		return;
 	
 	TextPtr = &PrimPtr->Object.Text;
-	origin.x = _save_stamp.origin.x;
+	origin.x = pClipRect->corner.x;
 	origin.y = TextPtr->baseline.y;
 	num_chars = TextPtr->CharCount;
 	if (num_chars == 0)
@@ -277,10 +276,9 @@ _text_blt (RECT *pClipRect, PRIMITIVE *PrimPtr)
 			r.corner.y = origin.y - fontChar->HotSpot.y;
 			r.extent.width = fontChar->disp.width;
 			r.extent.height = fontChar->disp.height;
-			_save_stamp.origin = r.corner;
 			if (BoxIntersect (&r, pClipRect, &r))
 			{
-				TFB_Prim_FontChar (&origin, fontChar, backing);
+				TFB_Prim_FontChar (origin, fontChar, backing, ctxOrigin);
 			}
 
 			origin.x += fontChar->disp.width;

src/libs/graphics/frame.c
@@ -48,43 +48,51 @@ typedef struct
 	INTERNAL_PRIM_DESC Object;
 } INTERNAL_PRIMITIVE;
 
-STAMP _save_stamp;
 
-static BOOLEAN
-GetFrameValidRect (RECT *pValidRect, HOT_SPOT *pOldHot)
+// pValidRect or origin may be NULL
+BOOLEAN
+GetContextValidRect (RECT *pValidRect, POINT *origin)
 {
-	COORD hx, hy;
-	HOT_SPOT OldHot;
+	RECT tempRect;
+	POINT tempPt;
 
-	OldHot = _CurFramePtr->HotSpot;
-	hx = OldHot.x;
-	hy = OldHot.y;
-	pValidRect->corner.x = hx;
-	pValidRect->corner.y = hy;
-	pValidRect->extent.width = GetFrameWidth (_CurFramePtr);
-	pValidRect->extent.height = GetFrameHeight (_CurFramePtr);
+	if (!pValidRect)
+		pValidRect = &tempRect;
+	if (!origin)
+		origin = &tempPt;
+
+	// Start with a rect the size of foreground frame
+	pValidRect->corner.x = 0;
+	pValidRect->corner.y = 0;
+	pValidRect->extent = GetFrameBounds (_CurFramePtr);
+	*origin = _CurFramePtr->HotSpot;
+
 	if (_pCurContext->ClipRect.extent.width)
 	{
+		// If the cliprect is completely outside of the valid frame
+		// bounds we have nothing to draw
 		if (!BoxIntersect (&_pCurContext->ClipRect,
 				pValidRect, pValidRect))
 			return (FALSE);
 
-		hx -= _pCurContext->ClipRect.corner.x;
-		hy -= _pCurContext->ClipRect.corner.y;
-		pValidRect->corner.x += hx;
-		pValidRect->corner.y += hy;
-		_CurFramePtr->HotSpot = MAKE_HOT_SPOT (hx, hy);
+		// Foreground frame hotspot defines a drawing position offset
+		// WRT the context cliprect
+		origin->x += _pCurContext->ClipRect.corner.x;
+		origin->y += _pCurContext->ClipRect.corner.y;
 	}
 
-	*pOldHot = OldHot;
 	return (TRUE);
 }
 
-void
+static void
 ClearBackGround (RECT *pClipRect)
 {
+	RECT clearRect;
 	Color color = _get_context_bg_color ();
-	TFB_Prim_FillRect (pClipRect, color);
+	clearRect.corner.x = 0;
+	clearRect.corner.y = 0;
+	clearRect.extent = pClipRect->extent;
+	TFB_Prim_FillRect (&clearRect, color, pClipRect->corner);
 }
 
 void
@@ -92,9 +100,9 @@ DrawBatch (PRIMITIVE *lpBasePrim, PRIM_LINKS PrimLinks,
 		BATCH_FLAGS BatchFlags)
 {
 	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (&ValidRect, &origin))
 	{
 		COUNT CurIndex;
 		PRIM_LINKS OldLinks;
@@ -145,42 +153,38 @@ DrawBatch (PRIMITIVE *lpBasePrim, PRIM_LINKS PrimLinks,
 			{
 				case POINT_PRIM:
 					color = GetPrimColor (lpWorkPrim);
-					TFB_Prim_Point (&lpWorkPrim->Object.Point, color);
+					TFB_Prim_Point (&lpWorkPrim->Object.Point, color, origin);
 					break;
 				case STAMP_PRIM:
-					TFB_Prim_Stamp (&lpWorkPrim->Object.Stamp);
+					TFB_Prim_Stamp (&lpWorkPrim->Object.Stamp, origin);
 					break;
 				case STAMPFILL_PRIM:
 					color = GetPrimColor (lpWorkPrim);
-					TFB_Prim_StampFill (&lpWorkPrim->Object.Stamp, color);
+					TFB_Prim_StampFill (&lpWorkPrim->Object.Stamp, color,
+							origin);
 					break;
 				case LINE_PRIM:
 					color = GetPrimColor (lpWorkPrim);
-					TFB_Prim_Line (&lpWorkPrim->Object.Line, color);
+					TFB_Prim_Line (&lpWorkPrim->Object.Line, color, origin);
 					break;
 				case TEXT_PRIM:
-					if (!TextRect (&lpWorkPrim->Object.Text,
-							&ClipRect, NULL))
+					if (!TextRect (&lpWorkPrim->Object.Text, &ClipRect, NULL))
 						continue;
-
-					_save_stamp.origin = ClipRect.corner;
-					
-					_text_blt (&ClipRect, lpWorkPrim);
+					// ClipRect is relative to origin
+					_text_blt (&ClipRect, lpWorkPrim, origin);
 					break;
 				case RECT_PRIM:
 					color = GetPrimColor (lpWorkPrim);
-					TFB_Prim_Rect (&lpWorkPrim->Object.Rect, color);
+					TFB_Prim_Rect (&lpWorkPrim->Object.Rect, color, origin);
 					break;
 				case RECTFILL_PRIM:
 					color = GetPrimColor (lpWorkPrim);
-					TFB_Prim_FillRect (&lpWorkPrim->Object.Rect, color);
+					TFB_Prim_FillRect (&lpWorkPrim->Object.Rect, color, origin);
 					break;
 			}
 		}
 
 		UnbatchGraphics ();
-
-		_CurFramePtr->HotSpot = OldHot;
 
 		if (BatchFlags & BATCH_SINGLE)
 			SetPrimLinks (lpBasePrim,
@@ -192,98 +196,81 @@ void
 ClearDrawable (void)
 {
 	RECT ValidRect;
-	HOT_SPOT OldHot;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (&ValidRect, NULL))
 	{
-		BatchGraphics ();
 		ClearBackGround (&ValidRect);
-		UnbatchGraphics ();
-
-		_CurFramePtr->HotSpot = OldHot;
 	}
 }
 
 void
 DrawPoint (POINT *lpPoint)
 {
-	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (NULL, &origin))
 	{
 		Color color = GetPrimColor (&_locPrim);
-		TFB_Prim_Point (lpPoint, color);
-		_CurFramePtr->HotSpot = OldHot;
+		TFB_Prim_Point (lpPoint, color, origin);
 	}
 }
 
 void
 DrawRectangle (RECT *lpRect)
 {
-	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (NULL, &origin))
 	{
 		Color color = GetPrimColor (&_locPrim);
-		TFB_Prim_Rect (lpRect, color);
-		_CurFramePtr->HotSpot = OldHot;
+		TFB_Prim_Rect (lpRect, color, origin);
 	}
 }
 
 void
 DrawFilledRectangle (RECT *lpRect)
 {
-	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (NULL, &origin))
 	{
 		Color color = GetPrimColor (&_locPrim);
-		TFB_Prim_FillRect (lpRect, color);
-		_CurFramePtr->HotSpot = OldHot;
+		TFB_Prim_FillRect (lpRect, color, origin);
 	}
 }
 
 void
 DrawLine (LINE *lpLine)
 {
-	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (NULL, &origin))
 	{
 		Color color = GetPrimColor (&_locPrim);
-		TFB_Prim_Line (lpLine, color);
-		_CurFramePtr->HotSpot = OldHot;
+		TFB_Prim_Line (lpLine, color, origin);
 	}
 }
 
 void
 DrawStamp (STAMP *stmp)
 {
-	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (NULL, &origin))
 	{
-		TFB_Prim_Stamp (stmp);
-		_CurFramePtr->HotSpot = OldHot;
+		TFB_Prim_Stamp (stmp, origin);
 	}
 }
 
 void
 DrawFilledStamp (STAMP *stmp)
 {
-	RECT ValidRect;
-	HOT_SPOT OldHot;
+	POINT origin;
 
-	if (GraphicsSystemActive () && GetFrameValidRect (&ValidRect, &OldHot))
+	if (GraphicsSystemActive () && GetContextValidRect (NULL, &origin))
 	{
 		Color color = GetPrimColor (&_locPrim);
-		TFB_Prim_StampFill (stmp, color);
-		_CurFramePtr->HotSpot = OldHot;
+		TFB_Prim_StampFill (stmp, color, origin);
 	}
 }
 

src/libs/graphics/tfb_prim.c
@@ -29,12 +29,12 @@
 #include "libs/log.h"
 
 void
-TFB_Prim_Point (POINT *p, Color color)
+TFB_Prim_Point (POINT *p, Color color, POINT ctxOrigin)
 {
 	RECT r;
 
-	r.corner.x = p->x - _CurFramePtr->HotSpot.x;
-	r.corner.y = p->y - _CurFramePtr->HotSpot.y;
+	r.corner.x = p->x + ctxOrigin.x;
+	r.corner.y = p->y + ctxOrigin.y;
 	r.extent.width = r.extent.height = 1;
 
 	if (_CurFramePtr->Type == SCREEN_DRAWABLE)
@@ -44,7 +44,7 @@ TFB_Prim_Point (POINT *p, Color color)
 }
 
 void
-TFB_Prim_Rect (RECT *r, Color color)
+TFB_Prim_Rect (RECT *r, Color color, POINT ctxOrigin)
 {
 	RECT arm;
 	int gscale;
@@ -52,30 +52,30 @@ TFB_Prim_Rect (RECT *r, Color color)
 	arm = *r;
 	arm.extent.width = r->extent.width;
 	arm.extent.height = 1;
-	TFB_Prim_FillRect (&arm, color);
+	TFB_Prim_FillRect (&arm, color, ctxOrigin);
 	arm.extent.height = r->extent.height;
 	arm.extent.width = 1;
-	TFB_Prim_FillRect (&arm, color);
+	TFB_Prim_FillRect (&arm, color, ctxOrigin);
 	// rounding error correction here
 	arm.corner.x += ((r->extent.width * gscale + (GSCALE_IDENTITY >> 1))
 			/ GSCALE_IDENTITY) - 1;
-	TFB_Prim_FillRect (&arm, color);
+	TFB_Prim_FillRect (&arm, color, ctxOrigin);
 	arm.corner.x = r->corner.x;
 	arm.corner.y += ((r->extent.height * gscale + (GSCALE_IDENTITY >> 1))
 			/ GSCALE_IDENTITY) - 1;
 	arm.extent.width = r->extent.width;
 	arm.extent.height = 1;
-	TFB_Prim_FillRect (&arm, color);	
+	TFB_Prim_FillRect (&arm, color, ctxOrigin);
 }
 
 void
-TFB_Prim_FillRect (RECT *r, Color color)
+TFB_Prim_FillRect (RECT *r, Color color, POINT ctxOrigin)
 {
 	RECT rect;
 	int gscale;
 
-	rect.corner.x = r->corner.x - _CurFramePtr->HotSpot.x;
-	rect.corner.y = r->corner.y - _CurFramePtr->HotSpot.y;
+	rect.corner.x = r->corner.x + ctxOrigin.x;
+	rect.corner.y = r->corner.y + ctxOrigin.y;
 	rect.extent.width = r->extent.width;
 	rect.extent.height = r->extent.height;
 
@@ -97,14 +97,14 @@ TFB_Prim_FillRect (RECT *r, Color color)
 }
 
 void
-TFB_Prim_Line (LINE *line, Color color)
+TFB_Prim_Line (LINE *line, Color color, POINT ctxOrigin)
 {
 	int x1, y1, x2, y2;
 
-	x1=line->first.x - _CurFramePtr->HotSpot.x;
-	y1=line->first.y - _CurFramePtr->HotSpot.y;
-	x2=line->second.x - _CurFramePtr->HotSpot.x;
-	y2=line->second.y - _CurFramePtr->HotSpot.y;
+	x1=line->first.x + ctxOrigin.x;
+	y1=line->first.y + ctxOrigin.y;
+	x2=line->second.x + ctxOrigin.x;
+	y2=line->second.y + ctxOrigin.y;
 
 	if (_CurFramePtr->Type == SCREEN_DRAWABLE)
 		TFB_DrawScreen_Line (x1, y1, x2, y2, color, TFB_SCREEN_MAIN);
@@ -113,7 +113,7 @@ TFB_Prim_Line (LINE *line, Color color)
 }
 
 void
-TFB_Prim_Stamp (STAMP *stmp)
+TFB_Prim_Stamp (STAMP *stmp, POINT ctxOrigin)
 {
 	int x, y;
 	FRAME SrcFramePtr;
@@ -140,8 +140,8 @@ TFB_Prim_Stamp (STAMP *stmp)
 	LockMutex (img->mutex);
 
 	img->NormalHs = SrcFramePtr->HotSpot;
-	x = stmp->origin.x - _CurFramePtr->HotSpot.x;
-	y = stmp->origin.y - _CurFramePtr->HotSpot.y;
+	x = stmp->origin.x + ctxOrigin.x;
+	y = stmp->origin.y + ctxOrigin.y;
 
 	if (TFB_DrawCanvas_IsPaletted(img->NormalImg) && img->colormap_index != -1)
 	{
@@ -162,7 +162,7 @@ TFB_Prim_Stamp (STAMP *stmp)
 }
 
 void
-TFB_Prim_StampFill (STAMP *stmp, Color color)
+TFB_Prim_StampFill (STAMP *stmp, Color color, POINT ctxOrigin)
 {
 	int x, y;
 	FRAME SrcFramePtr;
@@ -188,8 +188,8 @@ TFB_Prim_StampFill (STAMP *stmp, Color color)
 	LockMutex (img->mutex);
 
 	img->NormalHs = SrcFramePtr->HotSpot;
-	x = stmp->origin.x - _CurFramePtr->HotSpot.x;
-	y = stmp->origin.y - _CurFramePtr->HotSpot.y;
+	x = stmp->origin.x + ctxOrigin.x;
+	y = stmp->origin.y + ctxOrigin.y;
 
 	UnlockMutex (img->mutex);
 
@@ -205,12 +205,13 @@ TFB_Prim_StampFill (STAMP *stmp, Color color)
 }
 
 void
-TFB_Prim_FontChar (POINT *origin, TFB_Char *fontChar, TFB_Image *backing)
+TFB_Prim_FontChar (POINT charOrigin, TFB_Char *fontChar, TFB_Image *backing,
+		POINT ctxOrigin)
 {
 	int x, y;
 
-	x = origin->x - _CurFramePtr->HotSpot.x;
-	y = origin->y - _CurFramePtr->HotSpot.y;
+	x = charOrigin.x + ctxOrigin.x;
+	y = charOrigin.y + ctxOrigin.y;
 
 	if (_CurFramePtr->Type == SCREEN_DRAWABLE)
 	{

src/libs/graphics/tfb_prim.h
@@ -20,11 +20,11 @@
 #include "tfb_draw.h"
 
 
-void TFB_Prim_Line (LINE *line, Color color);
-void TFB_Prim_Point (POINT *p, Color color);
-void TFB_Prim_Rect (RECT *r, Color color);
-void TFB_Prim_FillRect (RECT *r, Color color);
-void TFB_Prim_Stamp (STAMP *stamp);
-void TFB_Prim_StampFill (STAMP *stamp, Color color);
-void TFB_Prim_FontChar (POINT *origin, TFB_Char *fontChar,
-		TFB_Image *backing);
+void TFB_Prim_Line (LINE *line, Color color, POINT ctxOrigin);
+void TFB_Prim_Point (POINT *p, Color color, POINT ctxOrigin);
+void TFB_Prim_Rect (RECT *r, Color color, POINT ctxOrigin);
+void TFB_Prim_FillRect (RECT *r, Color color, POINT ctxOrigin);
+void TFB_Prim_Stamp (STAMP *stamp, POINT ctxOrigin);
+void TFB_Prim_StampFill (STAMP *stamp, Color color, POINT ctxOrigin);
+void TFB_Prim_FontChar (POINT charOrigin, TFB_Char *fontChar,
+		TFB_Image *backing, POINT ctxOrigin);

commit 845f8e139ca1bb0c1fe84f64c45b0a618177ba8c

Better context drawing origin semantics; also chaser fix for foreground frame hotspot left broken in r3401
git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@3407 8092fc87-c524-0410-9efc-e669fe64eaf9

src/libs/gfxlib.h
@@ -164,6 +164,13 @@ typedef struct line
 	POINT first, second;
 } LINE;
 
+static inline POINT
+MAKE_POINT (COORD x, COORD y)
+{
+	POINT pt = {x, y};
+	return pt;
+}
+
 static inline bool
 pointsEqual (POINT p1, POINT p2)
 {
@@ -278,6 +285,8 @@ extern BOOLEAN SetContextClipRect (RECT *pRect);
 // The returned rect is always filled in. If the context cliprect
 // is undefined, the returned rect has foreground frame dimensions.
 extern BOOLEAN GetContextClipRect (RECT *pRect);
+// The actual origin will be orgOffset + context ClipRect.corner
+extern POINT SetContextOrigin (POINT orgOffset);
 
 extern TIME_VALUE DrawablesIntersect (INTERSECT_CONTROL *pControl0,
 		INTERSECT_CONTROL *pControl1, TIME_VALUE max_time_val);

src/libs/graphics/context.c
@@ -249,6 +249,12 @@ GetContextClipRect (RECT *lpRect)
 	return (_pCurContext->ClipRect.extent.width != 0);
 }
 
+POINT
+SetContextOrigin (POINT orgOffset)
+{
+	// XXX: This is a hack, kind of. But that's what the original did.
+	return SetFrameHot (_CurFramePtr, orgOffset);
+}
 
 FRAME
 SetContextFontEffect (FRAME EffectFrame)

src/uqm/melee.c
@@ -363,6 +363,7 @@ RepairMeleeFrame (RECT *pRect)
 	RECT r;
 	CONTEXT OldContext;
 	RECT OldRect;
+	POINT oldOrigin;
 
 	r.corner.x = pRect->corner.x + SAFE_X;
 	r.corner.y = pRect->corner.y + SAFE_Y;
@@ -376,8 +377,9 @@ RepairMeleeFrame (RECT *pRect)
 	OldContext = SetContext (SpaceContext);
 	GetContextClipRect (&OldRect);
 	SetContextClipRect (&r);
-	SetFrameHot (Screen,
-			MAKE_HOT_SPOT (r.corner.x - SAFE_X, r.corner.y - SAFE_Y));
+	// Offset the origin so that we draw the correct gfx in the cliprect
+	oldOrigin = SetContextOrigin (MAKE_POINT (-r.corner.x + SAFE_X,
+			-r.corner.y + SAFE_Y));
 	BatchGraphics ();
 
 	DrawMeleeIcon (0);   /* Entire melee screen */
@@ -413,7 +415,7 @@ RepairMeleeFrame (RECT *pRect)
 		DrawPickFrame (pMeleeState);
 		
 	UnbatchGraphics ();
-	SetFrameHot (Screen, MAKE_HOT_SPOT (0, 0));
+	SetContextOrigin (oldOrigin);
 	SetContextClipRect (&OldRect);
 	SetContext (OldContext);
 }

src/uqm/planets/plangen.c
@@ -128,10 +128,10 @@ RenderTopography (FRAME DstFrame, BYTE *pTopoData, int w, int h)
 		BYTE *pSrc;
 		BYTE *xlat_tab;
 		BYTE *cbase;
-		HOT_SPOT OldHot;
+		POINT oldOrigin;
 		RECT ClipRect;
 
-		OldHot = SetFrameHot (DstFrame, MAKE_HOT_SPOT (0, 0));
+		oldOrigin = SetContextOrigin (MAKE_POINT (0, 0));
 		GetContextClipRect (&ClipRect);
 		SetContextClipRect (NULL);
 
@@ -201,7 +201,7 @@ RenderTopography (FRAME DstFrame, BYTE *pTopoData, int w, int h)
 		}
 
 		SetContextClipRect (&ClipRect);
-		SetFrameHot (DstFrame, OldHot);
+		SetContextOrigin (oldOrigin);
 	}
 
 	SetContextFGFrame (OldFrame);

src/uqm/planets/pstarmap.c
@@ -287,6 +287,7 @@ DrawStarMap (COUNT race_update, RECT *pClipRect)
 	COUNT which_space;
 	long diameter;
 	RECT r, old_r;
+	POINT oldOrigin;
 	STAMP s;
 	FRAME star_frame;
 	STAR_DESC *SDPtr;
@@ -312,8 +313,9 @@ DrawStarMap (COUNT race_update, RECT *pClipRect)
 		SetContextClipRect (pClipRect);
 		pClipRect->corner.x -= old_r.corner.x;
 		pClipRect->corner.y -= old_r.corner.y;
-		SetFrameHot (Screen,
-				MAKE_HOT_SPOT (pClipRect->corner.x, pClipRect->corner.y));
+		// Offset the origin so that we draw the correct gfx in the cliprect
+		oldOrigin = SetContextOrigin (MAKE_POINT (-pClipRect->corner.x,
+				-pClipRect->corner.y));
 	}
 
 	if (transition_pending)
@@ -548,7 +550,7 @@ printf ("%s\n", buf);
 	if (pClipRect)
 	{
 		SetContextClipRect (&old_r);
-		SetFrameHot (Screen, MAKE_HOT_SPOT (0, 0));
+		SetContextOrigin (oldOrigin);
 	}
 
 	if (race_update == 0)

src/uqm/setup.c
@@ -107,7 +107,7 @@ LoadKernel (int argc, char *argv[])
 
 	SetContext (ScreenContext);
 	SetContextFGFrame (Screen);
-	SetFrameHot (Screen, MAKE_HOT_SPOT (0, 0));
+	SetContextOrigin (MAKE_POINT (0, 0));
 
 	hResIndex = (RESOURCE_INDEX) InitResourceSystem ();
 	if (hResIndex == 0)

src/uqm/shipstat.c
@@ -139,6 +139,8 @@ InitShipStatus (SHIP_INFO *SIPtr, STARSHIP *StarShipPtr, RECT *pClipRect)
 	COORD y = 0; // default, for Melee menu
 	STAMP Stamp;
 	CONTEXT OldContext;
+	RECT oldClipRect;
+	POINT oldOrigin;
 
 	if (StarShipPtr) // set during battle
 	{
@@ -149,14 +151,16 @@ InitShipStatus (SHIP_INFO *SIPtr, STARSHIP *StarShipPtr, RECT *pClipRect)
 	OldContext = SetContext (StatusContext);
 	if (pClipRect)
 	{
-		GetContextClipRect (&r);
+		GetContextClipRect (&oldClipRect);
+		r = oldClipRect;
 		r.corner.x += pClipRect->corner.x;
 		r.corner.y += (pClipRect->corner.y & ~1);
 		r.extent = pClipRect->extent;
 		r.extent.height += pClipRect->corner.y & 1;
 		SetContextClipRect (&r);
-		SetFrameHot (Screen, MAKE_HOT_SPOT (
-				pClipRect->corner.x, (pClipRect->corner.y & ~1)));
+		// Offset the origin so that we draw into the cliprect
+		oldOrigin = SetContextOrigin (MAKE_POINT (-pClipRect->corner.x,
+				-(pClipRect->corner.y & ~1)));
 	}
 
 	BatchGraphics ();
@@ -291,12 +295,8 @@ InitShipStatus (SHIP_INFO *SIPtr, STARSHIP *StarShipPtr, RECT *pClipRect)
 
 	if (pClipRect)
 	{
-		SetFrameHot (Screen, MAKE_HOT_SPOT (0, 0));
-		r.corner.x = SPACE_WIDTH + SAFE_X;
-		r.corner.y = SAFE_Y;
-		r.extent.width = STATUS_WIDTH;
-		r.extent.height = STATUS_HEIGHT;
-		SetContextClipRect (&r);
+		SetContextOrigin (oldOrigin);
+		SetContextClipRect (&oldClipRect);
 	}
 
 	SetContext (OldContext);

src/uqm/util.c
@@ -144,7 +144,7 @@ PauseGame (void)
 	STAMP s;
 	CONTEXT OldContext;
 	FRAME F;
-	HOT_SPOT OldHot;
+	POINT oldOrigin;
 	RECT OldRect;
 
 	if (ActivityFrame == 0
@@ -159,7 +159,7 @@ PauseGame (void)
 
 	LockMutex (GraphicsLock);
 	OldContext = SetContext (ScreenContext);
-	OldHot = SetFrameHot (Screen, MAKE_HOT_SPOT (0, 0));
+	oldOrigin = SetContextOrigin (MAKE_POINT (0, 0));
 	GetContextClipRect (&OldRect);
 	SetContextClipRect (NULL);
 
@@ -204,7 +204,7 @@ PauseGame (void)
 	ClearSystemRect ();
 
 	SetContextClipRect (&OldRect);
-	SetFrameHot (Screen, OldHot);
+	SetContextOrigin (oldOrigin);
 	SetContext (OldContext);
 
 	WaitForNoInput (ONE_SECOND / 4, TRUE);