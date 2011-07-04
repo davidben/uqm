fc00ff56a74109c4c28f6670e8226bfd140688da
avolkov <avolkov@8092fc87-c524-0410-9efc-e669fe64eaf9>
2011-07-04 16:44:22 +0000

    Version 0.7.0
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@3628 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/uqmversion.h
+++ src/uqmversion.h
@@ -23,7 +23,7 @@
 #define UQM_MINOR_VERSION_S  "7"
 #define UQM_PATCH_VERSION     0
 #define UQM_PATCH_VERSION_S  "0"
-#define UQM_EXTRA_VERSION    "-rc2"
+#define UQM_EXTRA_VERSION    ""
 /* The final version is interpreted as:
  * printf ("%d.%d.%d%s", UQM_MAJOR_VERSION, UQM_MINOR_VERSION,
  * 		UQM_PATCH_VERSION, UQM_EXTRA_VERSION);
