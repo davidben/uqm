2009cf6b43009d439c9a95a6538144552545cf6c
avolkov <avolkov@8092fc87-c524-0410-9efc-e669fe64eaf9>
2011-07-09 03:49:12 +0000

    Make the switch from positional -> not positional sfx actually work w/o having sfx channels permanently stuck somewhere in 3D world; there is no reason to use different distance models, and that eliminates the need to change models when switching positional <-> not
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@3639 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/libs/sound/openal/audiodrv_openal.c
+++ src/libs/sound/openal/audiodrv_openal.c
@@ -193,10 +193,7 @@ openAL_Init (audio_Driver *driver, sint32 flags)
 		return -1;
 	}
 	
-	if (optStereoSFX)
-		alDistanceModel (AL_INVERSE_DISTANCE);
-	else
-		alDistanceModel (AL_NONE);
+	alDistanceModel (AL_INVERSE_DISTANCE);
 
 	(void) driver; // eat compiler warning
 
