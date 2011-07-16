09dafc3329aa508914878e6051a6d9384393f662
avolkov <avolkov@8092fc87-c524-0410-9efc-e669fe64eaf9>
2011-07-16 16:26:57 +0000

    Fix a type warning in sdl sound driver that could actually become a problem
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@3648 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/libs/sound/mixer/sdl/audiodrv_sdl.c
+++ src/libs/sound/mixer/sdl/audiodrv_sdl.c
@@ -90,6 +90,8 @@ static const audio_Driver mixSDL_Driver =
 };
 
 
+static void audioCallback (void *userdata, Uint8 *stream, int len);
+
 /*
  * Initialization
  */
@@ -143,7 +145,7 @@ mixSDL_Init (audio_Driver *driver, sint32 flags)
 
 	desired.format = AUDIO_S16SYS;
 	desired.channels = 2;
-	desired.callback = mixer_MixChannels;
+	desired.callback = audioCallback;
 	
 	log_add (log_Info, "Opening SDL audio device.");
 	if (SDL_OpenAudio (&desired, &obtained) < 0)
@@ -247,6 +249,11 @@ mixSDL_Uninit (void)
 	SDL_QuitSubSystem (SDL_INIT_AUDIO);
 }
 
+static void
+audioCallback (void *userdata, Uint8 *stream, int len)
+{
+	mixer_MixChannels (userdata, stream, len);
+}
 
 /*
  * General
