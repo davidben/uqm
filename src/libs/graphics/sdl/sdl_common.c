4ec6946e8833c22fb1b7fb37a7dd18becbf5296c
Meep-Eep <Meep-Eep@8092fc87-c524-0410-9efc-e669fe64eaf9>
2011-12-28 18:31:23 +0000

    Revert; SDL_PollEvent() never returns -1.
    git-svn-id: https://sc2.svn.sourceforge.net/svnroot/sc2/trunk/sc2@3714 8092fc87-c524-0410-9efc-e669fe64eaf9

--- src/libs/graphics/sdl/sdl_common.c
+++ src/libs/graphics/sdl/sdl_common.c
@@ -186,7 +186,7 @@ TFB_ProcessEvents ()
 {
 	SDL_Event Event;
 
-	while (SDL_PollEvent (&Event) > 0)
+	while (SDL_PollEvent (&Event))
 	{
 		/* Run through the InputEvent filter. */
 		ProcessInputEvent (&Event);
