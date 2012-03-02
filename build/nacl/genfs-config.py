5cb5305b8e55c56a7dc5cb12732bfab6743213cb
David Benjamin <davidben@mit.edu>
2012-03-02 02:09:28 -0500

    Include non-comm fonts in main pack
    There are very few of them. It's not worth the extra pack.

--- build/nacl/genfs-config.py
+++ build/nacl/genfs-config.py
@@ -28,20 +28,9 @@ packs.append([['base/nav/', '*', '']])
 packs.append([['base/planets/', '*', '']])
 
 # Separate comm files. Pack with matching font.
-comm = set(get_dirs(os.path.join(srcdir, 'base/comm')))
-for c in comm:
+for c in get_dirs(os.path.join(srcdir, 'base/comm')):
     packs.append([['base/fonts/%s.fon' % c, '*', ''],
                   ['base/comm/%s' % c, '*', '']])
-
-# Pack remaining non-startup fonts together.
-startup_fonts = ('starcon', 'tiny', 'micro')
-font_pack = []
-for f in get_dirs(os.path.join(srcdir, 'base/fonts')):
-    if f.endswith('.fon'):
-        base = f[:-len('.fon')]
-        if base not in comm and base not in startup_fonts:
-            font_pack.append(['base/fonts/%s' % f, '*', ''])
-packs.append(font_pack)
 
 # Pack 3domusic separately.
 packs.append([['addons/3domusic', '*', '*.rmp']])
